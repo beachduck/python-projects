43883e372b3e0945f20a4ba563879ea23cb6c91c
beachduck <147434735+beachduck@users.noreply.github.com>
2023-10-09 12:47:15 -0700

    Updated code to only allow positive whole numbers

--- perfect square.py
+++ perfect square.py
@@ -1,6 +1,18 @@
 import math
 
-n = input("Enter a number to find the square root:")
+while True:
+    print('Enter a positive whole number to find the square root')
+    n = input()
+    try:
+        n = int(n)
+    except:
+        print('Please use a whole number')
+        continue
+    if n < 1:
+        print('Please use a positive number')
+        continue
+    break
+
 y = math.sqrt(int(n))
 z = float (y).is_integer()
     
@@ -9,3 +21,4 @@ if z > 0:
     print (' Wow! ' + str(n) + ' is a perfect square!')
 elif z <= 0:
         print('Oh no, ' + str(n) + ' is not a perfect square.')
+        
